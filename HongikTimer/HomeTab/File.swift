39def9d7ed858c97dd31a4a403f8ae0e2df29257
JongHoon <qnm541@gmail.com>
2022-05-10 12:18:49 +0900

    홈탭 - 그래프 cell구현, 홈 cell 높이 수정, 배경 화면 구현

---
+++ HongikTimer/HomeTab/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  HongikTimer
+//
+//  Created by JongHoon on 2022/05/10.
+//
+
+import Foundation
---
+++ HongikTimer/HomeTab/TimeGraphCell.swift
@@ -0,0 +1,23 @@
+//
+//  TimeGraphCell.swift
+//  HongikTimer
+//
+//  Created by JongHoon on 2022/05/10.
+//
+
+import UIKit
+
+class TimeGraphCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
